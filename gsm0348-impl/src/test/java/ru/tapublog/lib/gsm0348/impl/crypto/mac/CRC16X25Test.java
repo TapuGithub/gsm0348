e78a7f44ece1000962e1384d32e0f990b5904524
Pim Moerenhout <pim.moerenhout@gmail.com>
2017-11-11 22:33:05 +0100

    Add CRC16 testcase from ETSI 102 225.

--- gsm0348-impl/src/test/java/ru/tapublog/lib/gsm0348/impl/crypto/mac/CRC16X25Test.java
+++ gsm0348-impl/src/test/java/ru/tapublog/lib/gsm0348/impl/crypto/mac/CRC16X25Test.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 
 public class CRC16X25Test {
 
+  // ETSI 102 225
+  // If an input message is '01 02 03 04 05' where '01' is the first byte and '05' the last byte used for the
+  // computation, then the result of CRC 16 computation applied to the input message is '22 EC', where
+  // '22' would represent the first byte and 'EC' the last byte of the RC/CC/DS field.
+
   @Test
   public void test_crc16_x25() throws Exception {
     CRC16X25 crc16X25 = new CRC16X25();
     crc16X25.init(null);
-    final byte[] data = new byte[]{ (byte) 0xFF, (byte) 0x10, (byte) 0x9A, (byte) 0x45, (byte) 0xF2, 0x7B, (byte) 0xF1, (byte) 0x00 };
+    final byte[] data = new byte[]{ (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04, (byte) 0x05 };
     crc16X25.update(data, 0, data.length);
     final byte[] crc = new byte[2];
     final int bytesCopied = crc16X25.doFinal(crc, 0);
     assertEquals(2, bytesCopied);
-    assertArrayEquals(new byte[]{ (byte) 0xCF, (byte) 0x0A }, crc);
+    assertArrayEquals(new byte[]{ (byte) 0x22, (byte) 0xEC }, crc);
   }
 }
--- gsm0348-impl/src/test/java/ru/tapublog/lib/gsm0348/impl/crypto/mac/CRC32Test.java
+++ gsm0348-impl/src/test/java/ru/tapublog/lib/gsm0348/impl/crypto/mac/CRC32Test.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 
 public class CRC32Test {
 
-  // 0x01 0x02 0x03 0x04 0x05
+  // ETSI 102 225
+  // If an input message is '01 02 03 04 05' where '01' is the first byte and '05' the last byte used for the
+  // computation, then the result of CRC 32 computation applied to the input message is
+  // '47 0B 99 F4', where '47' would represent the first byte and 'F4' the last byte of the RC/CC/DS field.
+
   @Test
   public void test_crc32() throws Exception {
     CRC32 crc32 = new CRC32();
